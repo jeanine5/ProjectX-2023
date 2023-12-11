f638cbaecf03ca892e9b31ba1f2544605aa80b02
jeanineohene-agyei <j.oheneagyei@mail.utoronto.ca>
2023-12-10 22:55:57 -0500

    dataset

---
+++ EcoNAS/Benchmark/generate_cifar_dataset.py
@@ -0,0 +1,26 @@
+"""
+
+"""
+from torchvision.datasets import CIFAR10
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+from EcoNAS.Benchmark.search_space import BenchmarkDataset
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+transform = transforms.ToTensor()
+
+train_dataset = CIFAR10(root='../data', train=True, transform=transform, download=True)
+test_dataset = CIFAR10(root='../data', train=False, transform=transform)
+
+batch_size = 128
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+benchmark_dataset = BenchmarkDataset()
+architectures = benchmark_dataset.generate_architectures(max_hidden_layers=20, max_hidden_size=200)
+benchmark_dataset.evaluate_architectures(architectures, train_loader, test_loader)
+benchmark_dataset.store_results_to_csv(filename='cifar10_benchmark_results.csv')
--- EcoNAS/Benchmark/search_space.py
+++ EcoNAS/Benchmark/search_space.py
@@ -5,9 +5,6 @@ import random
 import csv
 
 from EcoNAS.EA.Architectures import *
-from torchvision.datasets import MNIST
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
 
 
 class BenchmarkDataset:
@@ -47,7 +44,7 @@ class BenchmarkDataset:
             self.results.append(result)
             i += 1
 
-    def store_results_to_csv(self, filename='mnist_benchmark_results.csv'):
+    def store_results_to_csv(self, filename):
         """
         Store the benchmark results in a CSV file
         """
@@ -63,20 +60,5 @@ class BenchmarkDataset:
 
             for result in self.results:
                 writer.writerow(result)
-
-
-transform = transforms.ToTensor()
-
-train_dataset = MNIST(root='../data', train=True, transform=transform, download=True)
-test_dataset = MNIST(root='../data', train=False, transform=transform)
-
-batch_size = 128
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
-
-benchmark_dataset = BenchmarkDataset()
-architectures = benchmark_dataset.generate_architectures(max_hidden_layers=20, max_hidden_size=200)
-benchmark_dataset.evaluate_architectures(architectures, train_loader, test_loader)
-benchmark_dataset.store_results_to_csv()
 
 
